d34ea61ff55ef89ba31efab2767faf3f031f03f6
kart1KRK <kartikkhiratkar1264@gmail.com>
2024-01-30 20:38:49 +0530

    intro to javascript

---
+++ java script/1.introduction to java script/script.js
@@ -0,0 +1,11 @@
+let c=12;
+let d=13;
+
+let age = 20 //decleration and initialization;
+const name ='kartik'
+
+// console.log(age)
+console.log('c+d is', c+d);
+age = 30;
+console.log('age',age);
+console.log('name',name);
