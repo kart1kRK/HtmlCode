887b2a9a8ef8a2f3a2fa7c86e62a007b3d8b8ce8
kart1KRK <kartikkhiratkar1264@gmail.com>
2024-02-07 23:40:37 +0530

    loops

---
+++ java script/6.loops in js/script.js
@@ -0,0 +1,35 @@
+// for (let index = 1;index<6;){
+//     console.log("kartik",index);
+// }
+
+// for (let index = 1;index<6;index=index +2){
+//     console.log("kartik",index);
+// }
+
+// for (let index = 1;index<0;index=index +3){
+//     console.log("kartik",index);
+// }
+
+// let step = 0;
+// while (step<5){
+//     console.log("step",step);
+//     step += 1 ;
+// }
+
+
+// let step = 6;
+// do{
+//     console.log("step",step);
+//     step += 1;
+// }while (step<5);
+
+let step = 0;
+while (step<5){
+    step += 1 ;
+    if (step === 2){
+        continue;
+    }
+    console.log("step",step);
+}
+
+
