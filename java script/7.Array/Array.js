bed5f2524b21faac894eb97837959078b6a7f18b
kart1KRK <kartikkhiratkar1264@gmail.com>
2024-02-08 23:12:47 +0530

    Dead Streak

---
+++ java script/7.Array/Array.js
@@ -0,0 +1,24 @@
+const cars = ["Saab", "Volvo", "BMW"];
+
+const fourWhee = [
+    "Saab",
+    "Volvo",
+    "BMW"
+  ];
+
+cars = [];
+cars[0]= "Saab";
+cars[1]= "Volvo";
+cars[2]= "BMW";
+
+const len = cars.length   // Returns the number of elements
+cars.sort()   // Sorts the array
+
+// looping using the map function
+const fun = () =>{
+    cars.map(() => (
+        console.log(car)
+    ))
+}
+
+fun()
