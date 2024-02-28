e02799cc3671e87ee3ee167424fce6f1b784f07d
kart1KRK <kartikkhiratkar1264@gmail.com>
2024-02-28 07:40:51 +0530

    new project

--- java script/7.Array/Array.js
+++ java script/7.Array/Array.js
@@ -1,24 +1,40 @@
-const cars = ["Saab", "Volvo", "BMW"];
+const words =["apple"
+,"bannana"
+,"mango"
+,12
+,true,
+{
+    name: "kartik",
+},
+function hello() {
+    console.log("hello World");
+},
+// console.log(words);  
+console.log(words[6]);
 
-const fourWhee = [
-    "Saab",
-    "Volvo",
-    "BMW"
-  ];
 
-cars = [];
-cars[0]= "Saab";
-cars[1]= "Volvo";
-cars[2]= "BMW";
 
-const len = cars.length   // Returns the number of elements
-cars.sort()   // Sorts the array
+// const cars = ["Saab", "Volvo", "BMW"];
 
-// looping using the map function
-const fun = () =>{
-    cars.map(() => (
-        console.log(car)
-    ))
-}
+// const fourWhee = [
+//     "Saab",
+//     "Volvo",
+//     "BMW"
+//   ];
 
-fun()
+// cars = [];
+// cars[0]= "Saab";
+// cars[1]= "Volvo";
+// cars[2]= "BMW";
+
+// const len = cars.length   // Returns the number of elements
+// cars.sort()   // Sorts the array
+
+// // looping using the map function
+// const fun = () =>{
+//     cars.map(() => (
+//         console.log(car)
+//     ))
+// }
+
+// fun()
